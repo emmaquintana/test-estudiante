0316ee7d71eb2c8bd59744e9a66ab063db8bf775
Emmanuel Quintana <eqb2021@gmail.com>
2023-12-29 19:30:15 -0300

    feat: Introduces loading spinner when a page is loading

--- src/app/(Pages)/Questions/Result/loading.tsx
+++ src/app/(Pages)/Questions/Result/loading.tsx
@@ -1,3 +1,21 @@
+import {CgSpinner} from 'react-icons/cg'; 
+import animations from '@/assets/animations/animations.module.css';
+
 export default function Loading() {
-    return <h1>Loading</h1>
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100dvh'
+        }}>
+            <CgSpinner 
+                className={animations.rotate} 
+                style={{
+                    fontSize: '10vw',
+                    color: 'black'
+                }}
+            />            
+        </div>
+    );
 }
