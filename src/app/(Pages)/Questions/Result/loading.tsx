8f8c956f3702efbfdd6a702d2fa7ee9dee0abb98
Emmanuel Quintana <eqb2021@gmail.com>
2023-12-30 08:25:22 -0300

    chore: Remove unnecessary imports in all loading.tsx files

--- src/app/(Pages)/Questions/Result/loading.tsx
+++ src/app/(Pages)/Questions/Result/loading.tsx
@@ -1,5 +1,3 @@
-import {CgSpinner} from 'react-icons/cg'; 
-import animations from '@/assets/animations/animations.module.css';
 import LoadingScreen from '@/assets/components/LoadingScreen';
 
 export default function Loading() {
