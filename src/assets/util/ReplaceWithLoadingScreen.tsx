106b7a042c8fe0c01f64a05c0524ecdee175d087
Emmanuel Quintana <eqb2021@gmail.com>
2023-12-31 16:10:06 -0300

    feat: Create a function that given a ref, replaces the HTMLElement with the <LoadingScreen /> component

---
+++ src/assets/util/ReplaceWithLoadingScreen.tsx
@@ -0,0 +1,14 @@
+import { RefObject } from "react";
+import { createRoot } from "react-dom/client";
+import LoadingScreen from "../components/LoadingScreen";
+import animations from '@/assets/animations/animations.module.css';
+
+export default function replaceWithLoadingScreen(ref: RefObject<HTMLElement>, animation: boolean = true) {
+    if (ref.current) {        
+        createRoot(ref.current).render(<LoadingScreen />);        
+        if (animation) {
+            ref.current.classList.remove(animations.fadeInToOut);
+            ref.current.classList.add(animations.fadeOutToIn);
+        }
+    }
+}
