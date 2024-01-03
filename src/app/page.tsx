e89f2a3d04d954e4743162fd427007af7438eb51
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-03 17:04:27 -0300

    fix: push.router() is executed with useEffect(). This is cuz 'location is not referenced' error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
     
     const router = useRouter();    
     
-    return (
-        <>
-            {router.push('/Home')}
-        </>
-    );
+    useEffect(() => {
+        // It loads once the page is rendered
+        {router.push('/Home')}
+    }, []);
+    
+    return;
 }
