fa7fadfa56f81e07076251b12c841069aaeb5ecb
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-03 18:36:04 -0300

    feat: Add a loading screen in app/page.tsx with replaceLoadingScreen() function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import replaceWithLoadingScreen from "@/assets/util/Functions/ReplaceWithLoadingScreen";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
     
     const router = useRouter();    
+    const divRef = useRef<HTMLDivElement>(null);
     
     useEffect(() => {
         // It loads once the page is rendered
+        replaceWithLoadingScreen(divRef);
         {router.push('/Home')}
     }, []);
     
-    return;
+    return (
+        <div ref={divRef}></div>
+    );
 }
