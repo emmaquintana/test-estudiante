dff57f78fe2b8a88e815c03d0424943d5db52fdf
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-03 17:05:17 -0300

    chore: Code moved for legibility

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true"
+    enabled: process.env.ANALYZE == "true"
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer(
-    {reactStrictMode: true}
-);
-
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
