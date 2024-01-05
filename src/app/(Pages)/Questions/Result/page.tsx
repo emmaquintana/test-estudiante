4e29aa3c87a1a918224e37a055b03d42a6aad538
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-04 21:59:06 -0300

    feat: To /Result: Add styles. The button redirect to /Home

--- src/app/(Pages)/Questions/Result/page.tsx
+++ src/app/(Pages)/Questions/Result/page.tsx
@@ -1,16 +1,41 @@
 "use client"
 
 import animations from '@/assets/animations/animations.module.css'
-import { useSearchParams } from 'next/navigation';
+import styles from './page.module.css';
+import getResult from '@/assets/util/Functions/GetResult';
+import { useSearchParams, useRouter } from 'next/navigation';
+import Button from '@/assets/components/Button';
+import replaceWithLoadingScreen from '@/assets/util/Functions/ReplaceWithLoadingScreen';
+import { ElementRef, useRef } from 'react';
 
 export default function Result() {
 
     const searchParams = useSearchParams();
-    const score = searchParams.get("score");
-    
+    const score = Number(searchParams.get("score"));    
+    const result = getResult(score);
+    const router = useRouter();
+    const sectionRef = useRef<ElementRef<"section">>(null);
+
+    function handleClick() {
+        replaceWithLoadingScreen(sectionRef);        
+        router.push('/Home');
+    }
+
     return (
-        <section className={`${animations.fadeAppear}`}>
-            Resultado: {score}
+        <section className={`${animations.fadeAppear} ${styles.container}`} ref={sectionRef}>
+            <main className={`${styles.resultContainer}`}>
+                <h1 className={`${styles.text}`}>Resultado</h1>
+                <h2 className={`${styles.text}`}>{result.title}</h2>                                    
+                <p className={`${styles.text} ${styles.desc}`}>{result.desc}</p>                
+                <Button 
+                    variant='light' 
+                    fontSize='25px' 
+                    onClick={handleClick}
+                    className={styles.btn}
+                >
+                    Finalizar Test
+                </Button>
+            </main>            
         </section>
     );
 }
