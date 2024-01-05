0c2b6777289b8e97a6ba5349c10fae9ee8c7c79d
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-04 21:30:52 -0300

    refactor: Update type's imports (Question, Answer)

--- src/assets/util/Info/QA.ts
+++ src/assets/util/Info/QA.ts
@@ -1,23 +1,4 @@
-type Question = {
-    id: number,
-    desc: string,
-    options?: Answer[],
-    correctAnswerIndex?: number[] | number
-}
-
-type Answer = {
-    id: number,
-    desc: string,
-    score?: number
-    //The score could follow the following rules:
-    //10: Perfect
-    //5: Regular
-    //0: Bad
-    //This is in case we want to qualify the answer
-    //by the quality and not by correct/wrong.
-    //This is useful for many cases such as some tests with questions like
-    //"How do you prepare yourself for a test/examen?"
-}
+import { Question } from "../Types/Question";
 
 export const Questions: Question[] = [
     {
